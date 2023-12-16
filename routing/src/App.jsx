98e746ac7e448ed90c56bb1bfbfb083e3705dc93
BALASHNEKITHAA SATHEESHKUMAR <shnekithaasatheesh2004@gmail.com>
2023-12-16 16:42:20 +0530

    Done

---
+++ routing/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Allrouter from './Components/Allrouter';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Allrouter />
+    </>
+  );
+}
+
+export default App;
